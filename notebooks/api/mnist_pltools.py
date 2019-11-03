e8c3f059f4d6ad2f41673a0bc4b36b74631bb331
mibaumgartner <m.baumgartner@ymail.com>
2019-11-03 22:19:51 +0100

    started evaluating some ideas for tta and ensembling

--- notebooks/api/mnist_pltools.py
+++ notebooks/api/mnist_pltools.py
@@ -24,13 +24,15 @@ class MNIST_Wrapper():
     def __getitem__(self, index):
         sample = self.dset[index]
         return {'data': np.array(sample[0])[None].astype(np.float32),
-                'label': sample[1]}
+                'label': sample[1],
+                'id': f'sample{index}'}
 
 
 dset_dict = MNIST_Wrapper(dset)
 dset_transformer = Transformer(dset_dict,
                                [MirrorTransform((0,)),
-                                ToTensor(dtypes=(
+                                ToTensor(keys=('data', 'label'),
+                                         dtypes=(
                                     ('data', torch.float32),
                                     ('label', torch.int64)))])
 # %%
@@ -41,18 +43,22 @@ dset_transformer[1]['data'].shape
 # %%
 import torch
 from torch.nn import functional as F
-from torch.utils.data import DataLoader
-import pytorch_lightning as pl
+
+from pltools.train.module import PLTModule
 
 
-class CoolSystem(pl.LightningModule):
-    def __init__(self):
-        super(CoolSystem, self).__init__()
-        # not the best model...
+class LinearModel(torch.nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.l1 = torch.nn.Linear(28 * 28, 10)
 
     def forward(self, x):
         return torch.relu(self.l1(x.view(x.size(0), -1)))
+
+
+class CoolSystem(PLTModule):
+    def __init__(self, config, model=LinearModel()):
+        super().__init__(config=config, model=model)
 
     def training_step(self, batch, batch_nb):
         # REQUIRED
@@ -61,7 +67,8 @@ class CoolSystem(pl.LightningModule):
         y_hat = self.forward(x)
         loss = F.cross_entropy(y_hat, y)
         tensorboard_logs = {'train_loss': loss}
-        return {'loss': loss, 'log': tensorboard_logs}
+        return {'loss': loss,
+                'log': tensorboard_logs}
 
     def validation_step(self, batch, batch_nb):
         # OPTIONAL
@@ -82,30 +89,27 @@ class CoolSystem(pl.LightningModule):
         # (LBFGS it is automatically supported, no need for closure function)
         return torch.optim.Adam(self.parameters(), lr=0.02)
 
-    @pl.data_loader
-    def train_dataloader(self):
-        # REQUIRED
-        return DataLoader(dset_transformer, batch_size=32, num_workers=4)
-
-    @pl.data_loader
-    def val_dataloader(self):
-        # OPTIONAL
-        return DataLoader(dset_transformer, batch_size=32, num_workers=4)
-
-    @pl.data_loader
-    def test_dataloader(self):
-        # OPTIONAL
-        return DataLoader(dset_transformer, batch_size=32, num_workers=4)
-
 
 # %%
 from pytorch_lightning import Trainer
 
-model = CoolSystem()
+
+class Config():
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.dataloader = {'batch_size': 32, 'num_workers': 4}
+
+
+module = CoolSystem(Config())
+module.train_transformer = dset_transformer
+module.val_transformer = dset_transformer
+module.test_transformer = dset_transformer
+
+# %%
 
 # most basic trainer, uses good defaults
 trainer = Trainer()
-trainer.fit(model)
+trainer.fit(module)
 
 
 # %%
--- pltools/train/module.py
+++ pltools/train/module.py
@@ -1,1 +1,104 @@
-# TODO: improved module with more hp options and datasets
+import typing
+import torch
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+
+
+class PLTModule(pl.LightningModule):
+    def __init__(self, config, model, train_transformer=None,
+                 val_transformer=None, test_transformer=None, **kwargs):
+        super().__init__(**kwargs)
+        self.config = config
+        self.model = model
+        self.train_transformer = train_transformer
+        self.val_transformer = val_transformer
+        self.test_transformer = test_transformer
+
+    def forward(self, data: torch.Tensor, *args, **kwargs):
+        return self.model(data, *args, **kwargs)
+
+    @pl.data_loader
+    def train_dataloader(self):
+        if hasattr(self, "train_transformer"):
+            kwargs = self.get_dataloading_kwargs("train_dataloader")
+            return DataLoader(self.train_transformer, **kwargs)
+        else:
+            return super().train_dataloader()
+
+    @pl.data_loader
+    def val_dataloader(self):
+        if hasattr(self, "val_transformer"):
+            kwargs = self.get_dataloading_kwargs("val_dataloader")
+            return DataLoader(self.val_transformer, **kwargs)
+        else:
+            return super().val_dataloader()
+
+    @pl.data_loader
+    def test_dataloader(self):
+        if hasattr(self, "test_transformer"):
+            kwargs = self.get_dataloading_kwargs("test_dataloader")
+            return DataLoader(self.test_transformer, **kwargs)
+        else:
+            return super().test_dataloader()
+
+    def get_dataloading_kwargs(self, name):
+        if hasattr(self.config, name):
+            return getattr(self.config, name)
+        else:
+            return self.config.dataloader
+
+    def enable_tta_ensemble(self,
+                            trafos: typing.Iterable[typing.Callable] = (),
+                            inverse_trafos: typing.Iterable[typing.Callable] = None,
+                            tta_reduce: typing.Callable = None,
+                            models: typing.Iterable[torch.nn.Module] = None,
+                            ensemble_reduce: typing.Callable = None):
+        self._initial_forward = self.forward
+        self.forward = tta_ensemble_wrapper(self.forward, self,
+                                            trafos=trafos,
+                                            inverse_trafos=inverse_trafos,
+                                            tta_reduce=tta_reduce,
+                                            models=models,
+                                            ensemble_reduce=ensemble_reduce)
+
+    def disable_tta_ensemble(self, raise_error=False):
+        if hasattr(self, "_initial_forward"):
+            self.forward = self._initial_forward
+        elif not raise_error:
+            raise RuntimeError("TTA was not enabled!")
+        else:
+            pass
+
+
+def tta_ensemble_wrapper(func, module,
+                         trafos: typing.Iterable[typing.Callable] = (),
+                         inverse_trafos: typing.Iterable[typing.Callable] = None,
+                         tta_reduce: typing.Callable = None,
+                         models: typing.Iterable[torch.nn.Module] = None,
+                         ensemble_reduce: typing.Callable = None):
+    initial_model = module.model
+
+    def tta_ensemable_forward(self, data, *args, **kwargs):
+        ensemble_preds = []
+        for _model in models:
+            module.module = _model
+            tta_preds = []
+            for idx, _trafo in enumerate(trafos):
+                tta_data = _trafo(data) if _trafo is not None else data
+
+                tta_pred = func(tta_data, **kwargs)
+
+                if (inverse_trafos is not None and
+                        inverse_trafos[idx] is not None):
+                    tta_pred = inverse_trafos[idx](tta_pred)
+
+                tta_preds.append(tta_pred)
+
+                if tta_reduce is not None:
+                    tta_preds = tta_reduce(tta_preds)
+
+                ensemble_preds.append(tta_preds)
+        if tta_reduce is not None:
+            ensemble_preds = ensemble_reduce(ensemble_preds)
+    module.model = initial_model
+    return tta_ensemable_forward
